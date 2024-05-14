82dc3ef2c59d8c584508fbe31aa09a3e6417d6f2
APIVerve Team <hello@apiverve.com>
2024-05-14 15:42:06 -0500

    Bug fixes and Improvements

---
+++ stockmarketopentimesQueryOptions.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class stockmarketopentimesQueryOptions {
+public string exchange { get; set; }
+}
+
+}
