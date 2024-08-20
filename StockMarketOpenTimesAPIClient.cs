32fcad658cc6a7fd8cb0b10e963a03f0b3d051e0
APIVerve Team <hello@apiverve.com>
2024-08-20 11:08:58 -0500

    Bug Fixes

--- StockMarketOpenTimesAPIClient.cs
+++ StockMarketOpenTimesAPIClient.cs
@@ -57,7 +57,7 @@ namespace APIVerve
         /// </summary>
         /// <param name="callback"></param>
         /// <param name="options"></param>
-        public void ExecuteAsync(ExecuteAsyncCallback callback, stockmarketopentimesQueryOptions options = null)
+        public void ExecuteAsync(ExecuteAsyncCallback callback, StockMarketOpenTimesQueryOptions options = null)
         {
             ThreadPool.QueueUserWorkItem(state =>
             {
@@ -71,7 +71,7 @@ namespace APIVerve
         /// </summary>
         /// <param name="options"></param>
         /// <returns></returns>
-        public ResponseObj Execute(stockmarketopentimesQueryOptions options = null)
+        public ResponseObj Execute(StockMarketOpenTimesQueryOptions options = null)
         {
             try
             {
@@ -89,6 +89,7 @@ namespace APIVerve
 
                 var request = WebRequest.Create(url);
                 request.Headers["x-api-key"] = _apiKey;
+                request.Headers["auth-mode"] = "nuget-package";
                 request.Method = _method;
 
                 if (_method == "POST")
@@ -149,7 +150,7 @@ namespace APIVerve
             }
         }
 
-        private string constructURL(stockmarketopentimesQueryOptions options)
+        private string constructURL(StockMarketOpenTimesQueryOptions options)
         {
             string url = _apiEndpoint;
 
